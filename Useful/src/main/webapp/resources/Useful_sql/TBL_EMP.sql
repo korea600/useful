340c7fe28b76ed4c69c0b2be5d92a6a560cdb04e
DaewonLee <DaewonLee@192.168.0.105>
2016-11-21 20:19:38 +0900

    사원정보/직책정보 기본데이터 삽입 (sql파일로 작성) + 조직도 DB접근용 세팅

--- Useful/src/main/webapp/resources/Useful_sql/TBL_EMP.sql
+++ Useful/src/main/webapp/resources/Useful_sql/TBL_EMP.sql
@@ -4,16 +4,16 @@ create table tbl_emp(
 empno integer primary key, --사번
 ename varchar2(10) not null, --사원명
 pass varchar2(20) not null, -- 비밀번호
-ssn varchar2(13) not null,   --주민번호
-phone varchar2(11),          --폰
-address varchar2(50),        --집주소
+ssn varchar2(15) not null,   --주민번호
+phone varchar2(15),          --폰
+address varchar2(100),        --집주소
 hiredate Date not null,      --입사일
 deptno integer ,          --부서번호
 position varchar2(10),       --직책
 manager varchar2(10),        --상사
 leave integer,               --잔여연차일
 account varchar2(20) not null, --급여계좌
-bank varchar2(10) not null,  -- 은핸
+bank varchar2(15) not null,  -- 은행
 sign varchar2(20)           --결제이미지
 );
 
---
+++ Useful/src/main/webapp/resources/Useful_sql/organization.sql
@@ -0,0 +1,24 @@
+-- 사원정보(tbl_emp) 샘플 데이터 삽입
+insert into tbl_emp (empno, ename, pass, ssn, phone, address, hiredate, deptno, position, manager, leave, account, bank)
+values (1000, '이길재', '1111','121212-1234567','010-123-4567','가산디지털',sysdate, 0, '사장',null, 30,'123456-7890123','한국은행');
+insert into tbl_emp (empno, ename, pass, ssn, phone, address, hiredate, deptno, position, manager, leave, account, bank)
+values (1001, '채효원', '1111','121212-1234567','010-123-4567','가산디지털',sysdate, 0, '부장',null, 30,'123456-7890123','한국은행');
+insert into tbl_emp (empno, ename, pass, ssn, phone, address, hiredate, deptno, position, manager, leave, account, bank)
+values (1002, '박지혜', '1111','121212-1234567','010-123-4567','가산디지털',sysdate, 0, '대리',null, 30,'123456-7890123','한국은행');
+insert into tbl_emp (empno, ename, pass, ssn, phone, address, hiredate, deptno, position, manager, leave, account, bank)
+values (1003, '안선영', '1111','121212-1234567','010-123-4567','가산디지털',sysdate, 0, '사원',null, 30,'123456-7890123','한국은행');
+insert into tbl_emp (empno, ename, pass, ssn, phone, address, hiredate, deptno, position, manager, leave, account, bank)
+values (1004, '이대원', '1111','121212-1234567','010-123-4567','가산디지털',sysdate, 0, '사원',null, 30,'123456-7890123','한국은행');
+insert into tbl_emp (empno, ename, pass, ssn, phone, address, hiredate, deptno, position, manager, leave, account, bank)
+values (1005, '전성원', '1111','121212-1234567','010-123-4567','가산디지털',sysdate, 0, '사원',null, 30,'123456-7890123','한국은행');
+-- 컬럼 데이터 크기 수정
+alter table tbl_emp modify (ssn varchar2(15));
+alter table tbl_emp modify (phone varchar2(15));
+alter table tbl_emp modify (bank varchar2(15));
+alter table tbl_emp modify (address varchar2(100));
+alter table tbl_position modify (position varchar2(20));
+-- 직책 테이블(tbl_position) 샘플데이터 삽입
+insert into tbl_position values ('사장', 0);
+insert into tbl_position values ('부장', 10);
+insert into tbl_position values ('대리', 20);
+insert into tbl_position values ('사원', 30);
