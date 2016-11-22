ad24e706c238d4a6e52e202d73b1e0bb429272a5
DaewonLee <DaewonLee@192.168.0.105>
2016-11-22 20:25:11 +0900

    결재테이블 수정 (첨부파일 컬럼 추가)

--- Useful/src/main/webapp/resources/Useful_sql/TBL_APPROVAL.sql
+++ Useful/src/main/webapp/resources/Useful_sql/TBL_APPROVAL.sql
@@ -1,4 +1,4 @@
-create table tbl_approval(
+create table tbl_approval(	-- 결재정보 테이블
 	no number primary key,	-- 문서번호
 	status varchar2(15),	-- 결재진행상태
 	title varchar2(100),	-- 문서 제목
@@ -7,5 +7,6 @@ create table tbl_approval(
 	regdate date default sysdate,	-- 작성일
 	enddate date,					-- 결재마감일
 	curr_approval varchar2(15),		-- 현재까지 완료된 결재선 (기안자와 같을 경우 아무도 결재하지 않은 상태)
-	next_approval varchar2(15)		-- 다음 결재자 (null이면 결재 완료)
+	next_approval varchar2(15),		-- 다음 결재자 (null이면 결재 완료)
+	addfile blob					-- 첨부파일 (blob : 최대 4GB의 binary 파일 저장)
 )
