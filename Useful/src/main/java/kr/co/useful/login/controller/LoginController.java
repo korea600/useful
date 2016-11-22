3ad27a11b43c92a0e9437cf0dc309579419aeaa5
CHAEHYOWON <CHAEHYOWON@192.168.0.160>
2016-11-22 10:11:44 +0900

    효원 : 스프링 등록

--- Useful/src/main/java/kr/co/useful/login/controller/LoginController.java
+++ Useful/src/main/java/kr/co/useful/login/controller/LoginController.java
@@ -1,5 +1,8 @@
 package kr.co.useful.login.controller;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class LoginController {
 
 }
--- Useful/src/main/java/kr/co/useful/login/persistence/LoginDAOImpl.java
+++ Useful/src/main/java/kr/co/useful/login/persistence/LoginDAOImpl.java
@@ -1,5 +1,8 @@
 package kr.co.useful.login.persistence;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class LoginDAOImpl implements LoginDAO {
 
 }
