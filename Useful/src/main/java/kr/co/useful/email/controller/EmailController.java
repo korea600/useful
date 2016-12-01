bbb5079f3d7ae134febce657ea565659e9642824
CHAEHYOWON <CHAEHYOWON@192.168.0.160>
2016-12-01 14:31:37 +0900

    효원

--- Useful/src/main/java/kr/co/useful/email/controller/EmailController.java
+++ Useful/src/main/java/kr/co/useful/email/controller/EmailController.java
@@ -21,7 +21,7 @@ public class EmailController {
         int num = EmailSender.generateNumber(6);
         
         String reciver = "boxer1992@naver.com"; //받을사람의 이메일입니다.
-        String subject = "임시 비밀번호입니다.";
+        String subject = "임시 비밀번호를 발송해드립니다.";
         String content = "임시 비밀번호는" + num + "입니다.";
       
          
