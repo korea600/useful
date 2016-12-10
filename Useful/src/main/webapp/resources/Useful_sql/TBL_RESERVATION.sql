3bfd476d8c886e7d198d1d3f8f5cce7c8a313b4f
안선영 <@192.168.1.3>
2016-12-10 10:07:30 +0900

    sql!!

--- Useful/src/main/webapp/resources/Useful_sql/TBL_RESERVATION.sql
+++ Useful/src/main/webapp/resources/Useful_sql/TBL_RESERVATION.sql
@@ -1,26 +1,23 @@
-create table tbl_reservation(
-serial number primary key, --일련번호
-roomno number not null, --회의실번호
-begin date not null, --시작시간
-end date not null, --종료시간
-booker varchar2(50), --예약자
-checked varchar2(20) --승인구분
-);
 
---에약자명 사원명 테이블에서 외래키 참조
 drop table tbl_reservation;
 create table tbl_reservation(
-serial number primary key, --일련번호
-roomno number not null, --회의실번호
-begin date not null, --시작시간
-end date not null, --종료시간
-ename varchar2(50) references tbl_emp(ename), --예약자
-checked varchar2(20) --승인구분
+serial number primary key, 
+roomno number not null, 
+beginday varchar2(30) not null, 
+begin number not null,
+end number not null,
+content varchar2(500),
+booker varchar2(50) not null, 
+empno number not null,
+checked varchar2(20) not null 
 );
 
-create sequence tbl_reservation_seq
-  start with 1
-   increment by 1
-   nocycle
-   nocache;
-   
+
+
+
+drop table tbl_roommake;
+create table tbl_roommake(
+ serial number primary key,
+ roomname varchar2(30) not null,
+ roomno number not null
+);
--- Useful/src/main/webapp/resources/Useful_sql/TBL_SCHEDULE.sql
+++ Useful/src/main/webapp/resources/Useful_sql/TBL_SCHEDULE.sql
@@ -1,13 +1,16 @@
+drop table tbl_schedule;
+
 create table tbl_schedule(
 serial number primary key, --글번호
-category varchar2(20) not null, --구분 -부서,전체
-deptno number not null, --부서번호
 title varchar2(50) not null, --제목
-content varchar2(2000), --글내용
-begin date not null, --시작 시간
-end date not null --종료시간
+content varchar2(2000),
+begin varchar2(30) not null, --시작 날짜
+end varchar2(30) not null, --종료날짜
+begintime number,
+regdate date default sysdate,
+deptno number not null,
+empno number not null
 );
-
 --부서번호 외래키 사용
 drop table tbl_schedule;
 create table tbl_schedule(
