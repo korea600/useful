00522828743ab68dceb0a2a45a64bcfa5755f1a9
DaewonLee <DaewonLee@192.168.0.170>
2016-12-13 01:54:14 +0900

    회의실예약파트 인터셉터 적용

---
+++ Useful/src/main/java/kr/co/useful/meeting/interceptor/MeetingManagerInterceptor.java
@@ -0,0 +1,41 @@
+package kr.co.useful.meeting.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import kr.co.useful.manager.domain.EmpVO;
+
+public class MeetingManagerInterceptor extends HandlerInterceptorAdapter{
+	
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		EmpVO vo = (EmpVO) request.getSession().getAttribute("LoginUser");
+		if(vo!=null){
+			if(vo.getEmpno()==1001)
+				return true;
+			else{
+				response.sendRedirect("/useful/access-control");
+				return false;
+			}
+		}	
+			
+		else{
+			saveDest(request);
+			request.getSession().removeAttribute("LoginUser");
+			response.sendRedirect("/useful");
+			return false;
+		}
+	}
+	
+	private void saveDest(HttpServletRequest request){	// 로그인여부 확인 전에 요청한 uri와 query저장
+		String uri = request.getRequestURI();
+		String query = request.getQueryString();	// getQueryString : 요청uri뒤에 붙는 '?'를 return
+		uri = uri.substring(uri.indexOf("/",2));
+		if(query==null)	query="";
+		else query="?"+query;
+		if(request.getMethod().equals("GET"))
+			request.getSession().setAttribute("dest", uri+query);	
+	}
+}
